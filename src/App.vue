10c18810f4d61a97b03ca6f4f6c5b4b698da3180
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-10 20:42:27 -0300

    Rework en favor al uso de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
-    <div id="nav">
-        <Menubar class="menu">
-            <template #start>
-                <div class="titulo">API-Pokémon</div>
-            </template>
-
-            <template #end>
-                <ul id="lista">
-                    <li v-for="(item, index) in items" :key="index">
-                        <router-link :to="item.to">{{item.label}}</router-link>
-                    </li>
-                </ul>
-            </template>
-        </Menubar>
-    </div>
+    <PokeNavBar :items="items"/>
     <router-view />
 </template>
 
 <script>
+import PokeNavBar from './components/PokeNavBar.vue'
 import { ref } from "vue";
-import Menubar from "primevue/menubar";
 
 export default {
     components: {
-        Menubar,
+        PokeNavBar,
     },
     setup() {
         const items = ref([
@@ -45,36 +31,3 @@ export default {
     },
 };
 </script>
-<style scoped>
-.titulo {
-    margin-left: 3vh;
-}
-
-.menu {
-    background-color: #7c7c7c;
-    font-weight: bold;
-    font-size: 2rem;
-    color: whitesmoke;
-    height: 7rem;
-}
-
-#lista li {
-     display:inline;
-     padding-left:3px;
-     padding-right:3px;
-     margin-right: 3vh;
-}
-
-#nav{
-    margin-bottom: 3vh;
-}
-
-#nav a {
-    text-decoration: none;
-    color: whitesmoke;
-}
-
-#nav a.router-link-exact-active {
-    color: #d3d3d3;
-}
-</style>
