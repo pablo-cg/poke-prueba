5f0cc779696345e3d0fd2f0ddd1fcc83c7f23761
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-10 16:19:53 -0300

    Agregado primevue al main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import PrimeVue from 'primevue/config'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/lara-light-teal/theme.css'
 
 const app = createApp(App)
 app.use(store)
