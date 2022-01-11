ac8e14305e2a872d7bbb26e3eef7227b4c7b9adf
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-11 16:20:57 -0300

    Creado el Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,40 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        pokeInfoList: [],
+        pokeFavList: [],
+    },
+    mutations: {
+        ADD_TO_INFOLIST(state, pokemon) {
+            state.pokeInfoList.push(pokemon)
+        },
+        REMOVE_FROM_INFOLIST(state, pokeName) {
+            state.pokeInfoList = state.pokeInfoList.filter(poke => poke.name != pokeName);
+        },
+        ADD_TO_FAVLIST(state, pokemon) {
+            state.pokeInfoList.push(pokemon)
+        }
+    },
+    actions: {
+        addPokeToList({ commit, state }, pokemon) {
+            const existingPoke = state.pokeInfoList.find(poke => poke.name == pokemon.name);
+            if (!existingPoke) {
+                commit('ADD_TO_INFOLIST', pokemon);
+            }
+        },
+
+        removePokeFromList({ commit }, pokeName) {
+            commit('REMOVE_FROM_INFOLIST', pokeName);
+        },
+
+        addPokeToFavList({ commit, state }, pokemon) {
+            const existingPoke = state.pokeFavList.find(poke => poke.name == pokemon.name);
+            if (!existingPoke) {
+                commit('ADD_TO_FAVLIST', pokemon);
+            }
+        }
+    },
+    modules: {
+    }
 })
