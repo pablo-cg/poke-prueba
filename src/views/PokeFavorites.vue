2bfd9ea0d07b338afe8a5bd1fb552eca9dd104ad
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-11 18:32:01 -0300

    Agregada la lógica de la vista de pokemones favoritos

--- src/views/PokeFavorites.vue
+++ src/views/PokeFavorites.vue
@@ -1,12 +1,44 @@
 <template>
-    <h1>favoritos</h1>
+    <div v-if="favoritePoke.length">
+        <h1>Tus Favoritos</h1>
+        <div style="margin-top: 0.5em">
+            <div class="p-grid">
+                <div
+                    class="p-col-6"
+                    v-for="pokemon in favoritePoke"
+                    :key="pokemon.id"
+                >
+                    <div class="box">
+                        <PokeFavoriteCard :pokemon="pokemon" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        <h1>No tienes pokemones favoritos</h1>
+    </div>
 </template>
 <script>
-
+import PokeFavoriteCard from "../components/PokeFavoriteCard.vue";
+import { computed } from "@vue/reactivity";
+import { useStore } from "vuex";
 export default {
-  name: 'Favoritos',
-  components: {
-   
-  }
-}
+    name: "PokeFavorites",
+    components: { PokeFavoriteCard },
+    setup() {
+        const store = useStore();
+
+        const favoritePoke = computed(() => store.state.pokeFavList);
+
+        return {
+            favoritePoke,
+        };
+    },
+};
 </script>
+<style scoped lang="scss">
+h1 {
+    font-weight: 100;
+}
+</style>
