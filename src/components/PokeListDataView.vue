cb72bd6cece1c49c57b5599241cc54d475d16241
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-10 22:56:26 -0300

    Nuevo componente que tiene la lista de los Pokemones

---
+++ src/components/PokeListDataView.vue
@@ -0,0 +1,76 @@
+<template>
+    <div v-if="pokemons">
+        <Fieldset legend="Lista de Pokemones">
+            <DataView :value="pokemons" :layout="layout" :rows="5">
+                <template #grid="slotProps">
+                    <div class="p-col-6">
+                        <div class="p-grid">
+                            <div class="p-col">
+                                <div class="box poke-name">
+                                    {{ slotProps.data.name }}
+                                </div>
+                            </div>
+                            <div class="p-col to-end">
+                                <div class="box">
+                                    <Button
+                                        label="Info"
+                                        icon="pi pi-info-circle"
+                                        class="p-button-info poke-info-btn"
+                                        @click="getInfo(slotProps.data)"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </DataView>
+        </Fieldset>
+        
+    </div>
+</template>
+
+<script>
+import Fieldset from "primevue/fieldset";
+import DataView from "primevue/dataview";
+import Button from "primevue/button";
+import { ref } from "@vue/reactivity";
+
+export default {
+    name: "Pokemones",
+    components: {
+        Fieldset,
+        DataView,
+        Button,
+    },
+    props: ["pokemons"],
+    setup() {
+        const layout = ref("grid");
+
+        function getInfo(pokeData) {
+            console.log(pokeData);
+        }
+
+        return {
+            layout,
+            getInfo,
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.to-end {
+    text-align: end;
+    padding-right: 20%;
+}
+
+.poke-info-btn {
+    background-color: #17a2b8 !important;
+    border-radius: 10px !important;
+}
+
+.poke-name {
+    text-transform: capitalize;
+    font-size: 1.3rem;
+}
+</style>
