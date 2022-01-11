3e45c2e29f01ec960de60a3837d804c157c289f6
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-11 16:24:05 -0300

    Uso de funciones flecha, agregado evento al dataview y estilos para los botones

--- src/views/PokeList.vue
+++ src/views/PokeList.vue
@@ -1,46 +1,54 @@
 <template>
     <div>
-        <PokeListDataView :pokemons="pokemons" />
+        <PokeListDataView :pokemons="pokemons" @getPokeInfo="getPokeInfo" />
         <div class="p-grid p-fluid poke-pages-btn">
             <div class="p-col-1" v-if="pageOffset > 0">
                 <div class="p-inputgroup">
                     <Button
                         label="Anterior"
-                        class="p-button-outlined"
+                        class="p-button-sm p-button-outlined poke-prev-btn"
                         @click="previousPage"
                     />
                 </div>
             </div>
             <div class="p-col">
                 <div class="p-inputgroup">
-                    <Button label="Siguiente" @click="nextPage" />
+                    <Button label="Siguiente" class="p-button-sm poke-next-btn" @click="nextPage" />
                 </div>
             </div>
         </div>
     </div>
+    <PokeListInfoTable />
 </template>
 
 <script>
 import PokeService from "../service/PokeService";
 import PokeListDataView from "../components/PokeListDataView.vue";
+import PokeListInfoTable from "../components/PokeListInfoTable.vue";
 import Button from "primevue/button";
 import { ref } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
+import { useStore } from "vuex";
 
 export default {
     name: "PokeList",
+
     components: {
         PokeListDataView,
+        PokeListInfoTable,
         Button,
     },
+
     setup() {
-        let pokemons = ref();
+        const pokemons = ref();
+        const pokeInfoList = ref([]);
         const crash = ref();
         const layout = ref("grid");
         const pageOffset = ref(0);
         const limitList = 10;
+        const store = useStore();
 
-        async function getPokemons(limit, offset) {
+        const getPokemons = async (limit, offset) => {
             try {
                 const { data, error } = await PokeService.getSomePokemons(
                     limit,
@@ -51,17 +59,27 @@ export default {
             } catch (error) {
                 crash.value = error;
             }
-        }
+        };
 
-        async function nextPage() {
+        const nextPage = async () => {
             await getPokemons(limitList, pageOffset.value + 10);
             pageOffset.value += 10;
-        }
+        };
 
-        async function previousPage() {
+        const previousPage = async () => {
             await getPokemons(limitList, pageOffset.value - 10);
             pageOffset.value -= 10;
-        }
+        };
+
+        const getPokeInfo = async (pokeName) => {
+            try {
+                const { data, error } = await PokeService.getPokemon(pokeName);
+                if (error) throw error;
+                store.dispatch('addPokeToList', data)
+            } catch (error) {
+                crash.value = error;
+            }
+        };
 
         onMounted(() => {
             getPokemons(limitList, pageOffset.value);
@@ -73,6 +91,8 @@ export default {
             pageOffset,
             nextPage,
             previousPage,
+            getPokeInfo,
+            pokeInfoList,
         };
     },
 };
@@ -80,5 +100,15 @@ export default {
 <style lang="scss" scoped>
 .poke-pages-btn {
     margin-top: 0.5rem;
+}
+
+::v-deep(.p-button-sm.p-button-outlined.poke-prev-btn){
+    color: #7c7c7c !important;
+    width: 110px !important;
+}
+
+::v-deep(.p-button-sm.poke-next-btn){
+    width: 110px !important;
+    background-color: #7c7c7c !important;
 }
 </style>
