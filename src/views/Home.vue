d4d8d82c6a657563f4e2bbdd486c314d7db7c40a
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-01-11 21:10:06 -0300

    Estilos para centrar la imagen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
-    <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1024px-International_Pok%C3%A9mon_logo.svg.png"
-        alt="PokemonLogo"
-        class="pokelogo"
-    />
+    <div class="logo-wrapper">
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1024px-International_Pok%C3%A9mon_logo.svg.png"
+            alt="PokemonLogo"
+            class="pokelogo"
+        />
+    </div>
 </template>
 
 <script>
@@ -13,9 +15,30 @@ export default {
 };
 </script>
 <style scoped>
-.pokelogo {
-    position: absolute;
-    inset: 0;
-    margin: auto;
+/* Gracias Google */
+.pokelogo
+{
+	position: absolute;
+	max-width: 80%;
+	top: 10%;
+	left: 10%;
+}
+.pokelogo:empty
+{
+	top: 50%;
+	left: 50%;
+	-webkit-transform: translate(-50%, -50%);
+	-moz-transform: translate(-50%, -50%);
+	-ms-transform: translate(-50%, -50%);
+	-o-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+}
+
+@media screen and (orientation: portrait) {
+  .pokelogo { max-width: 90%; }
+}
+
+@media screen and (orientation: landscape) {
+  .pokelogo { max-height: 90%; }
 }
 </style>
